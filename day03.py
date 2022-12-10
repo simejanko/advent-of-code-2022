5f4b2d507cddb6065a3045b9da3f514fcb408361
Simon Janezic <simon.janezic@gmail.com>
2022-12-10 07:44:40 +0100

    reformat

--- day03.py
+++ day03.py
@@ -5,8 +5,8 @@ PRIORITIES = {c: p + 1 for p, c in enumerate(string.ascii_letters)}
 
 with open(sys.argv[1], 'r') as f:
     backpacks = [l.strip() for l in f]
-    # part 1
-    print(sum([PRIORITIES[(set(items[:len(items) // 2]) & set(items[len(items) // 2:])).pop()] for items in backpacks]))
-    # part 2
-    print(sum([PRIORITIES[(set(backpacks[i]) & set(backpacks[i + 1]) & set(backpacks[i + 2])).pop()]
-               for i in range(0, len(backpacks) - 2, 3)]))
+# part 1
+print(sum([PRIORITIES[(set(items[:len(items) // 2]) & set(items[len(items) // 2:])).pop()] for items in backpacks]))
+# part 2
+print(sum([PRIORITIES[(set(backpacks[i]) & set(backpacks[i + 1]) & set(backpacks[i + 2])).pop()]
+           for i in range(0, len(backpacks) - 2, 3)]))
--- day06.py
+++ day06.py
@@ -4,18 +4,18 @@ from collections import Counter
 WINDOW_SIZE = 14
 with open(sys.argv[1], 'r') as f:
     s = f.read().strip()
-    window_counts = Counter(s[:WINDOW_SIZE])
-    for out_idx, in_c in enumerate(s[WINDOW_SIZE:]):
-        in_idx = out_idx + WINDOW_SIZE
-        out_c = s[out_idx]
+window_counts = Counter(s[:WINDOW_SIZE])
+for out_idx, in_c in enumerate(s[WINDOW_SIZE:]):
+    in_idx = out_idx + WINDOW_SIZE
+    out_c = s[out_idx]
 
-        if len(window_counts) == WINDOW_SIZE:
-            print(in_idx)
-            break
+    if len(window_counts) == WINDOW_SIZE:
+        print(in_idx)
+        break
 
-        window_counts[in_c] += 1
-        window_counts[out_c] -= 1
-        if window_counts[out_c] == 0:
-            del window_counts[out_c]
+    window_counts[in_c] += 1
+    window_counts[out_c] -= 1
+    if window_counts[out_c] == 0:
+        del window_counts[out_c]
 
 
